eacf0773ce7c6c8d083b59d9f8cd3c1780b83f5f
kilianklaiber <kilianklaiber@gmail.com>
2024-07-11 14:37:11 +0200

    cache ignored

---
+++ main.py
@@ -0,0 +1,12 @@
+import get_keys
+import tweepy
+
+
+
+# Your Twitter API credentials
+api_key = get_keys.get_api_key()
+api_secret_key = get_keys.get_api_key_secret()
+access_token = get_keys.get_access_token()
+access_token_secret = get_keys.get_access_token_secret()
+
+
