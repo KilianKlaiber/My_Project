43921ba9a4366bc600a5565a136a58a54c26ccd2
kilianklaiber <kilianklaiber@gmail.com>
2024-07-19 13:57:35 +0200

    formatted kilian_merge

--- kilian_merge.py
+++ kilian_merge.py
@@ -4,23 +4,22 @@ from random import shuffle
 
 
 def main():
-    
-    # Create unsortedlist    
+
+    # Create unsortedlist
     shuffled_list = list(range(10000))
     shuffle(shuffled_list)
-    
+
     # Call kilian_merge()
-    
+
     ordered_list = kilian_merge(shuffled_list)
-    
+
     print(ordered_list)
 
-    
+
 def kilian_merge(new_list):
 
-   # Create a list of ordered sublists:
+    # Create a list of ordered sublists:
     new_lists = lists_of_ordered_sublists(new_list)
-    
 
     # Merge the ordered sublists in order to create an ordered list using merge()
     while len(new_lists) >= 2:
@@ -28,7 +27,7 @@ def kilian_merge(new_list):
         left = new_lists.pop()
         interim_list = merge(left, right)
         new_lists.append(interim_list)
-    
+
     return new_lists[0]
 
 
